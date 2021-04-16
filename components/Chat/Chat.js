48e4791f2848acca99a6df6abbabd1d9ad91f790
Haxfx <sebastiaan@hotflux.nl>
2021-04-16 22:36:56 +0200

    feat(chat): added styling for chat card

--- components/Chat/Chat.js
+++ components/Chat/Chat.js
@@ -1,3 +1,4 @@
+import { DotsHorizontalIcon, PaperAirplaneIcon } from "@heroicons/react/solid";
 import React from "react";
 
 import { Message } from "./Message";
@@ -65,6 +66,14 @@ export const Chat = () => {
       <span className="self-left text-purple-light mt-4">
         Henk is typing...
       </span>
+      <div className="flex relative bg-purple-backgroundLight justify-start p-2 rounded-2xl mt-2">
+        <DotsHorizontalIcon className="w-7 h-7 p-1 rounded-full text-purple-medium bg-purple-light cursor-pointer hover:bg-white absolute left-2" />
+        <input
+          className="w-full px-10 bg-transparent text-purple-light focus:outline-none text-sm py-1"
+          placeholder="Write your message..."
+        />
+        <PaperAirplaneIcon className="w-7 h-7 p-1 rounded-full text-purple-white bg-purple-primary cursor-pointer hover:bg-white transform rotate-45 absolute right-2" />
+      </div>
     </div>
   );
 };
