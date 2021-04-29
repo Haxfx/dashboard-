843d0158bd647546f25dd28d699226ef06614c17
Haxfx <sebastiaan@hotflux.nl>
2021-04-30 00:43:18 +0200

    fix(font): updated font on loader

--- pages/loading/index.js
+++ pages/loading/index.js
@@ -2,7 +2,7 @@ export default function Index() {
   return (
     <div className="flex items-center justify-center m-0 p-0 w-screen h-screen text-sm text-center">
       <div className="edge3 animate-custom-spin before:edge1 after:edge2">
-        <span className="loading animate-reverse-spin">Loading...</span>
+        <span className="loading animate-reverse-spin text-xs">Loading...</span>
       </div>
     </div>
   );
