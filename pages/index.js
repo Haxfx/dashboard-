5b5f07f16e5271a2faa502fae0d9475834bb7801
Haxfx <sebastiaan@hotflux.nl>
2021-08-21 00:41:03 +0200

    fix(card): another attempt

--- pages/index.js
+++ pages/index.js
@@ -41,17 +41,22 @@ const Index = (props) => (
     <Head>
       <title>Stakeboard | Discovery</title>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@stakeboard" />
+      <meta name="twitter:creator" content="@stakeboard" />
       <meta
         name="twitter:text:title"
         content="Stakeboard connects delegators with SPO's"
       />
       <meta
+        name="twitter:image"
+        content="https://beta.stakeboard.net/stakeboard-logo.png"
+      ></meta>
+      <meta
         name="twitter:description"
         content="Stakeboard is the go-to trusted platform for SPOs and delegators to connect, stake, communicate, research, educate and collaborate on Cardano."
       />
-      <meta name="twitter:site" content="@stakeboard" />
-      <meta name="twitter:creator" content="@stakeboard" />
+
       <meta property="og:url" content="https://beta.stakeboard.net" />
       <meta
         property="og:title"
@@ -61,7 +66,10 @@ const Index = (props) => (
         property="og:description"
         content="Stakeboard is the go-to trusted platform for SPOs and delegators to connect, stake, communicate, research, educate and collaborate on Cardano."
       />
-      <meta property="og:image" content="/stakeboard-logo.png" />
+      <meta
+        property="og:image"
+        content="https://beta.stakeboard.net/stakeboard-logo.png"
+      />
     </Head>
     <div className="max-w-screen-2xl m-auto">
       <motion.div initial="initial" animate="animate" exit={{ opacity: 0 }}>
